9440152635b881752d4ca69e414a054807be96f4
JosephDelgadillo <delgadillo.jt@gmail.com>
2017-05-19 12:41:06 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import re
+
+print("Our Magical Calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
+
+    if equation == 'quit':
+        print("Goodbye, human.")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+
+while run:
+    performMath()
